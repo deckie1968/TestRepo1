547babdc1babe132be94da595927d9cbed3b103b
declan <declan.delaney@usa.net>
2018-03-17 13:07:17 +0100

    test feature commit

---
+++ Test C++ Project/src/Test C++ Project.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : Test.cpp
+// Author      : declan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	cout << "!!!test feature!!!" << endl; // prints !!test feature!!!
+
+	return 0;
+}
